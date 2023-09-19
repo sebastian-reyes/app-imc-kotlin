dc8799faca1656d49f987c7a77e58c86ac9b3c5e
sebastian-reyes <sebastianreyes2105@gmail.com>
2023-09-19 17:50:01 -0500

    lógica para cambiar color según que género pulsemos

--- app/src/main/java/com/sebastianreyes/appimc/MainActivity.kt
+++ app/src/main/java/com/sebastianreyes/appimc/MainActivity.kt
@@ -2,10 +2,60 @@ package com.sebastianreyes.appimc
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.cardview.widget.CardView
+import androidx.core.content.ContextCompat
 
 class MainActivity : AppCompatActivity() {
+
+    private var isMaleSelected: Boolean = true
+    private var isFemaleSelected: Boolean = false
+
+    private lateinit var viewMale: CardView
+    private lateinit var viewFemale: CardView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        initComponents()
+        initListeners()
+        initUI()
+    }
+
+    private fun initComponents() {
+        viewMale = findViewById(R.id.cvMasculino)
+        viewFemale = findViewById(R.id.cvFemenino)
+    }
+
+    private fun initListeners() {
+        viewMale.setOnClickListener {
+            changeGender()
+            setGenderColor()
+        }
+        viewFemale.setOnClickListener {
+            changeGender()
+            setGenderColor()
+        }
+    }
+
+    private fun changeGender() {
+        isMaleSelected = !isMaleSelected
+        isFemaleSelected = !isFemaleSelected
+    }
+
+    private fun setGenderColor() {
+        viewMale.setCardBackgroundColor(getBackgroundColor(isMaleSelected))
+        viewFemale.setCardBackgroundColor(getBackgroundColor(isFemaleSelected))
+    }
+
+    private fun getBackgroundColor(isSelectedComponent: Boolean): Int {
+        var colorReference = if (isSelectedComponent) {
+            R.color.background_component_selected
+        } else {
+            R.color.background_component
+        }
+        return ContextCompat.getColor(this, colorReference)
+    }
+
+    private fun initUI() {
+        setGenderColor()
     }
 }
